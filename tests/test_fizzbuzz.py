872b93217554a390caf9c672ae3c57516ef4dbe1
hhlei <leihenryh@gmail.com>
2024-12-04 00:20:08 -0700

    asdfasdfag

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -6,9 +6,13 @@ def test_playFizzBuzz_nominal():
     true_str = "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, Fizz Buzz"
     assert output_str == true_str
     
-def test_playFizzBuzz_non_int():
+def test_playFizzBuzz_string():
     with pytest.raises(TypeError) as e_info:
         output_str = playFizzBuzz("abc")
+        
+def test_playFizzBuzz_float():
+    with pytest.raises(TypeError) as e_info:
+        output_str = playFizzBuzz(0.1234152)
         
 def test_playFizzBuzz_non_positive():
     with pytest.raises(ValueError) as e_info:
