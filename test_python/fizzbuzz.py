53c214864b3061e7257acf17aeffcde5a3590f3e
hhlei <leihenryh@gmail.com>
2024-12-03 21:28:58 -0700

    Added testing script and made src as package

--- test_python/fizzbuzz.py
+++ test_python/fizzbuzz.py
@@ -1,6 +1,6 @@
 import numpy 
 
-if __name__ == "__main__":
+def printFizzBuzz(rounds):
     total = 20
     for i in range(1, total+1): #range is [start, stop)
         if (i % 3 == 0) and (i % 5==0):
@@ -11,3 +11,6 @@ if __name__ == "__main__":
             print("Buzz")
         else:
             print(i)
+
+if __name__ == "__main__":
+    printFizzBuzz(20)
